021a1dc62d9826491ce10d49edc86a9ee8a01eb7
Timothy Birrell <timothy_birrell@scbn.org>
2017-09-11 15:25:38 -0500

    added controller to handle non-unique actions

---
+++ src/Listeners/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wildside\Userstamps\Listeners;
+
+class Controller {
+    
+    public $created_by = 'created_by';
+    public $updated_by = 'updated_by';
+    public $deleted_by = 'deleted_by';
+    /**
+     * When the model is being created.
+     *
+     * @param Illuminate\Database\Eloquent\Model|static
+     * @return bool
+     */
+    public function prep($model)
+    {
+        if (! $model -> isUserstamping()) {
+            return false;
+        }
+    
+        if (defined(get_class($model).'::CREATED_BY')) $this->created_by = $model::CREATED_BY;
+        if (defined(get_class($model).'::UPDATED_BY')) $this->updated_by = $model::UPDATED_BY;
+        if (defined(get_class($model).'::DELETED_BY')) $this->deleted_by = $model::DELETED_BY;
+        
+        return true;
+    }
+}
